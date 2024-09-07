e08def73c68cb73ef46673bfac35a54112fc7a79
Marcin Rybkowski <marcin.rybkowski@enigma.com.pl>
2024-09-07 11:09:54 +0200

    homepage - about section - fix responsive mobile view

--- app/[locale]/sections/about/About.tsx
+++ app/[locale]/sections/about/About.tsx
@@ -19,30 +19,33 @@ export default function About() {
   return (
     <section id="about">
       <Container
-        className="overflow-hidden relative flex flex-col md:flex-row border gap-10 border-slate-400 dark:border-slate-50 rounded-[40px] p-10 my-10"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}>
-        <div className={`${isHovered ? 'visible' : 'invisible'}`}>
-          <BackgroundLayer
-            className="top-[-35%] right-[-40%] w-[80%] h-[80%]"
-            gradient={'purple'}
-          />
-          <BackgroundLayer
-            className="top-[-10%] left-[-40%] w-[80%] h-[80%]"
-            gradient={'green'}
-          />
-        </div>
-
-        <div className="md:w-1/2 w-full relative">
-          <div className="flex flex-row items-center gap-2">
-            <CameraIcon />
-            <H2>{t('title')}</H2>
+        <div className="overflow-hidden relative flex flex-col md:flex-row border gap-10 border-slate-400 dark:border-slate-50 rounded-[40px] p-10 my-10">
+          <div className={`${isHovered ? 'visible' : 'invisible'}`}>
+            <BackgroundLayer
+              className="top-[-35%] right-[-40%] w-[80%] h-[80%]"
+              gradient={'purple'}
+            />
+            <BackgroundLayer
+              className="top-[-10%] left-[-40%] w-[80%] h-[80%]"
+              gradient={'green'}
+            />
           </div>
-          <P className="text-7xl font-anekLatin">{t('subtitle')}</P>
-        </div>
-        <div className="flex flex-col gap-5 md:w-1/2 w-full">
-          <P>{t('content.0')}</P>
-          <P>{t('content.1')}</P>
+
+          <div className="md:w-1/2 w-full relative">
+            <div className="flex flex-row items-center gap-2">
+              <CameraIcon />
+              <H2>{t('title')}</H2>
+            </div>
+            <P className="text-5xl lg:text-7xl md:text-6xl font-anekLatin">
+              {t('subtitle')}
+            </P>
+          </div>
+          <div className="flex flex-col gap-5 md:w-1/2 w-full">
+            <P>{t('content.0')}</P>
+            <P>{t('content.1')}</P>
+          </div>
         </div>
       </Container>
     </section>
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -3,16 +3,19 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '../../lib/utils';
 
-const containerVariants = cva('flex flex-col mx-auto', {
-  variants: {
-    backgroundColor: {
-      default: 'transparent'
+const containerVariants = cva(
+  'flex flex-col mx-auto px-10 sm:px-0 max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl',
+  {
+    variants: {
+      backgroundColor: {
+        default: 'transparent'
+      }
+    },
+    defaultVariants: {
+      backgroundColor: 'default'
     }
-  },
-  defaultVariants: {
-    backgroundColor: 'default'
   }
-});
+);
 
 export interface ContainerProps
   extends React.HTMLProps<HTMLDivElement>,
@@ -26,10 +29,7 @@ const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
 
     return (
       <Comp
-        className={cn(
-          containerVariants({ backgroundColor, className }),
-          'max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl'
-        )}
+        className={cn(containerVariants({ backgroundColor, className }))}
         ref={ref}
         {...props}
       />
